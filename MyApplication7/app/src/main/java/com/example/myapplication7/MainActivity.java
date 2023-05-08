9da5970987aefb26617eff20bab27e37ad88a63a
eunekim <keh8992@gmail.com>
2023-05-08 13:48:40 +0900

    10장 어댑터뷰_그리드뷰 실습

---
+++ MyApplication7/app/src/main/java/com/example/myapplication7/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.myapplication7;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("그리드뷰 영화 포스터");
+
+        final GridView gv = (GridView) findViewById(R.id.gridView1);
+        MyGridAdapter gdapter = new MyGridAdapter(this);
+        gv.setAdapter(gdapter);
+    }
+
+    public class MyGridAdapter extends BaseAdapter {
+        Context context;
+        public MyGridAdapter(Context c){
+            context = c;
+        }
+        Integer[] posterID = {
+                R.drawable.mov01, R.drawable.mov02, R.drawable.mov03,
+                R.drawable.mov04, R.drawable.mov05, R.drawable.mov06,
+                R.drawable.mov07, R.drawable.mov08, R.drawable.mov09,
+                R.drawable.mov10,
+                R.drawable.mov01, R.drawable.mov02, R.drawable.mov03,
+                R.drawable.mov04, R.drawable.mov05, R.drawable.mov06,
+                R.drawable.mov07, R.drawable.mov08, R.drawable.mov09,
+                R.drawable.mov10,
+                R.drawable.mov01, R.drawable.mov02, R.drawable.mov03,
+                R.drawable.mov04, R.drawable.mov05, R.drawable.mov06,
+                R.drawable.mov07, R.drawable.mov08, R.drawable.mov09,
+                R.drawable.mov10,
+        };
+
+        public  int getCount(){
+            return posterID.length;
+        }
+
+        public Object getItem(int arg0){
+            return null;
+        }
+        public long getItemId(int arg0){
+            return 0;
+        }
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(200, 300));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5, 5, 5, 5);
+
+            imageView.setImageResource(posterID[position]);
+
+            final int pos = position;
+            imageView.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v) {
+                    View dialogView = (View) View.inflate(MainActivity.this,
+                            R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(posterID[pos]);
+                    dlg.setTitle("큰 포스터");
+                    dlg.setIcon(R.drawable.ic_launcher);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+
+            return imageView;
+        }
+    }
+}
